16bcded81568eac6416d22c19380fb97b1b97ba8
Nishant Kompella <63625756+infection-tag@users.noreply.github.com>
2022-07-11 13:43:27 -0700

    Create .helpme.c

---
+++ src/.helpme.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("Welcome to your new operating system!\n");
+    printf("To see a list of important recommended softwares, type soft.\n")    ;
+    printf("To run installation scripts, type sudo ninstall.\n");
+}
