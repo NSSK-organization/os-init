377316fd81680afd1ccccaf405dd5fe074252428
Nishant Kompella <63625756+infection-tag@users.noreply.github.com>
2022-07-11 18:23:07 -0700

    create file and add installations for brew and git

--- src/.ninstall-linux.c
+++ src/.ninstall-linux.c
@@ -1,3 +1,10 @@
 /* Note: We may run into some issues as some installations may not work on Debian-based systems. */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main() {
+    system("sh .brew-install.sh"); /* Install brew */
+    system("sh .git-install-linux.sh"); /* Install git */
+    /* TODO: Add scripts for installing Jupyter notebook and Python */
+}
