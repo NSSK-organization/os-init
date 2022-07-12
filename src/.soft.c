fc4c364988b31250ea8ea32f3c78985f902cbc65
Nishant Kompella <63625756+infection-tag@users.noreply.github.com>
2022-07-11 18:08:03 -0700

    add .soft.c

---
+++ src/.soft.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("running ninstall will install:\n\t");
+    printf("Jupyter Notebook and Python: Two important python tools\n\t");
+    printf("gcc: An important C compiler\n\t");
+    printf("Brew: A software package installer for MacOS and Linux\n\t");
+    printf("Git: An important way to back up your software\n");
+    printf("To get started, run ninstall. Then sit back and watch the magic happen.\n");
+}
