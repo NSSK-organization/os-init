e7f0af43f5348cf2e3dd26557782dcf07fbc2123
Nishant Kompella <63625756+infection-tag@users.noreply.github.com>
2022-07-11 18:14:48 -0700

    create file and add installations for brew and git

---
+++ src/.ninstall-mac.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    system("sh .brew-install.sh"); /* Install Brew */
+    system("sh .git-install-mac.sh"); /* Install Git for Mac */
+    /* TODO: Add installations for Jupyter Notebook and Python */
+}
