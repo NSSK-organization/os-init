d878cdc3940b575cfa09ae8a927f99ac56129beb
Nishant Kompella <63625756+infection-tag@users.noreply.github.com>
2022-07-12 18:11:58 -0700

    corrected src/ path

--- src/.ninstall-mac.c
+++ src/.ninstall-mac.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 int main() {
-    system("sh .brew-install.sh"); /* Install Brew */
-    system("sh .git-install-mac.sh"); /* Install Git for Mac */
-    system("sh .python-install-mac.sh"); /* Install Python 3 for Mac (Python 2 comes with it by default) */
-    system("sh .vim-install-mac.sh"); /* Install VIM and GVIM for mac */
-    system("sh .jupyter-install-mac.sh"); /* Install Jupyter Notebook for mac */
+    system("sh src/.brew-install.sh"); /* Install Brew */
+    system("sh src/.git-install-mac.sh"); /* Install Git for Mac */
+    system("sh src/.python-install-mac.sh"); /* Install Python 3 for Mac (Python 2 comes with it by default) */
+    system("sh src/.vim-install-mac.sh"); /* Install VIM and GVIM for mac */
+    system("sh src/.jupyter-install-mac.sh"); /* Install Jupyter Notebook for mac */
 }
