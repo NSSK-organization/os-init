70be769cbacbdddead92e57024bf84c42b99410a
Nishant Kompella <63625756+infection-tag@users.noreply.github.com>
2022-07-12 18:11:19 -0700

    Create .jupyter-install-mac.sh

---
+++ src/.jupyter-install-mac.sh
@@ -0,0 +1,6 @@
+# install jupyter notebook using pip3
+# Note: pip3 should come with python3 installation on mac
+
+osascript -e 'tell app "Terminal"
+    do script "pip3 install jupyter"
+end tell' 
