8a9facca12d4cb9b29de99bb80dea9dddb296ba5
Nishant Kompella <63625756+infection-tag@users.noreply.github.com>
2022-07-12 18:03:58 -0700

    added vim installer

--- src/.ninstall-mac.c
+++ src/.ninstall-mac.c
@@ -5,5 +5,6 @@ int main() {
     system("sh .brew-install.sh"); /* Install Brew */
     system("sh .git-install-mac.sh"); /* Install Git for Mac */
     system("sh .python-install-mac.sh"); /* Install Python 3 for Mac (Python 2 comes with it by default) */
+    system("sh .vim-install-mac.sh"); /* Install VIM and GVIM for mac */
     /* TODO: Add installations for Jupyter Notebook and GVIM */
 }
